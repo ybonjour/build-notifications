1c04582914d9dd8a9f326ed6be3da1f8aec37174
Yves Bonjour <yves.bonjour@ergon.ch>
2017-01-07 12:26:47 +0100

    Load spec_helper with rspec

--- spec/rules_parser_spec.rb
+++ spec/rules_parser_spec.rb
@@ -1,4 +1,4 @@
-require_relative 'spec_helper'
+require 'spec_helper'
 require 'build-notifications/rules_parser'
 require 'json'
 
--- test.sh
+++ test.sh
@@ -5,4 +5,4 @@ set -e
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
 # Mount workspace directly so the image does not always have to be rebuilt to run tests
-docker run --rm -v $dir:/workspace build-notifications bash -c "rspec /workspace/spec --color --format=documentation"
+docker run --rm -v $dir:/workspace build-notifications bash -c "(cd workspace; rspec spec --color --format=documentation)"
